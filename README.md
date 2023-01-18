644d8a056204d072c4d6923f8cf97a1a5b857bed
Emanuel Ibañes <ibanes.emanuel@gmail.com>
2023-01-18 20:42:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## _Trabajo Final Modulo 3_
+
+### *Resumen CV Responsivo*
+
+## Caracteristica del Proyecto
+- Usa tipografía de Google Fonts
+- Usa Boxicons para los iconos
+- Usa Eventos Js para seleccionar color, mostrar url y un scroll
+- Utilize la API https://api.open-meteo.com/en
+
+## Desarrollador
+
+>IBAÑES, EMANUEL ALEJANDRO
+
+
+## Tecnologia
+Es código abierto, el mismo se encuentra en un [repositorio público] en GitHub
+
+- [HTML]
+- [CSS]
+- [JS]
---
+++ assests/js/main.js
@@ -0,0 +1,39 @@
+/*==================== SELECT COLOR ====================*/
+document.querySelector('.switcher-btn').onclick = () => {
+    document.querySelector('.color-switcher').classList.toggle('active');
+};
+
+let themeButtons = document.querySelectorAll('.theme-buttons');
+
+themeButtons.forEach(color => {
+    color.addEventListener('click', () => {
+        let dataColor = color.getAttribute('data-color');
+        document.querySelector(':root').style.setProperty('--secondary', dataColor)
+    });
+});
+
+/*==================== SHOW URL ====================*/
+document.getElementById('mostrar_f').addEventListener('mouseover', function () {
+    document.getElementById("urlFace").innerHTML = "@Henderson";
+    var el = document.getElementById("social_f");
+    el.setAttribute("style", "cursor:pointer;");
+});
+
+document.getElementById('mostrar_l').addEventListener('mouseover', function () {
+    document.getElementById("urlLinkedin").innerHTML = "@Henderson";
+    var el = document.getElementById("social_l");
+    el.setAttribute("style", "cursor:pointer;");
+});
+document.getElementById('mostrar_g').addEventListener('mouseover', function () {
+    document.getElementById("urlGithub").innerHTML = "@Henderson";
+    var el = document.getElementById("social_g");
+    el.setAttribute("style", "cursor:pointer;");
+});
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
